fbb3638a27660493edd37c686c0c5ff04ce1c4fa
karticalm <121007023+karticalm@users.noreply.github.com>
2023-01-22 14:44:24 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,35 @@
+const heading1 = React.createElement(
+    "h1",
+    {
+      id: "heading1",
+      key: "heading1",
+    },
+    "Heading1"
+  );
+
+  const heading2 = React.createElement(
+    "h2",
+    {
+      id: "heading2",
+      key: "heading2",
+    },
+    "This is nesting elements under div through react"
+  );
+
+  const heading3 = React.createElement(
+    "h3",
+    {
+      id: "heading3",
+      key: "heading3",
+    },
+    "heading3"
+  );
+
+  const container = React.createElement("div", { id: "container" }, [
+    heading1,
+    heading2,
+    heading3,
+  ]);
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(container);
